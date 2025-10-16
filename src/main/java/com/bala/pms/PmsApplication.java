8fd847fa46c8929a08f5cabd9cad32a1b03a014c
Bala Kumar <balaashok2004@gmail.com>
2025-10-16 18:07:20 +0530

    Added docker compose file

--- src/main/java/com/bala/pms/PmsApplication.java
+++ src/main/java/com/bala/pms/PmsApplication.java
@@ -2,9 +2,15 @@ package com.bala.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class PmsApplication {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello world";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PmsApplication.class, args);
