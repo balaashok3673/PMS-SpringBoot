c458b325d7bc88551ae47533e391ba138fc265f3
Bala Kumar <balaashok2004@gmail.com>
2025-10-17 13:07:37 +0530

    Added service layer

---
+++ src/main/java/com/bala/pms/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.bala.pms.service;
+
+import com.bala.pms.model.ProductModel;
+import com.bala.pms.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface ProductService {
+    List<ProductModel> listProduct();
+    public ProductModel addProduct(ProductModel productModel);
+
+}
+
+@Service
+class ProductServiceImpl implements ProductService{
+
+    private final ProductRepository productRepository;
+
+    ProductServiceImpl(ProductRepository productRepository){
+        this.productRepository=productRepository;
+    }
+
+    @Override
+    public List<ProductModel> listProduct(){
+        return productRepository.findAll();
+    }
+
+    @Override
+    ProductModel addProduct(ProductModel productModel){
+       return productRepository.save(productModel);
+    }
+}
